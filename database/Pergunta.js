5c00d8a74118ae7417f289b68085e81fbcae1121
Igor Solfa Romero <igorsolfaromero@gmail.com>
2021-05-12 17:03:33 -0300

    Primeiro commit

---
+++ database/Pergunta.js
@@ -0,0 +1,18 @@
+//MODEL
+const Sequelize = require("sequelize");
+const connection = require("./database");
+
+const Pergunta = connection.define('perguntas',{
+    titulo:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    descricao:{
+        type: Sequelize.TEXT,
+        allowNull: false
+    }
+});
+
+Pergunta.sync({force: false}).then(()=>{});
+
+module.exports = Pergunta;
